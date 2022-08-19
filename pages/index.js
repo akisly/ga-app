e7a656bb825b8c06c22a56d1102906be1a58fa85
akisly <akisliy2306@gmail.com>
2022-08-19 11:07:09 +0300

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import ReactGA from 'react-ga';
 import styles from '../styles/Home.module.css'
+
+ReactGA.initialize('G-GEJLQ0ZRSQ');
 
 export default function Home() {
   return (
