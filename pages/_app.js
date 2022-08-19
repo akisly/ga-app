02bf6366528317e7953e887c099bf9bff7698603
akisly <akisliy2306@gmail.com>
2022-08-19 11:49:48 +0300

    ga - add pageview

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import ReactGA from 'react-ga4';
 import '../styles/globals.css'
 
 ReactGA.initialize('G-E16JC1JRC9');
+ReactGA.send("pageview");
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
